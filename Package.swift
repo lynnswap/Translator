13bb4f920de6adbe110d0bd31bddf08ad97b513e
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2026-03-02 09:47:26 +0900

    build(package): add strictSwiftSettings

--- Package.swift
+++ Package.swift
@@ -19,11 +19,18 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Translator"
+            name: "Translator",
+            swiftSettings: strictSwiftSettings,
         ),
         .testTarget(
             name: "TranslatorTests",
-            dependencies: ["Translator"]
+            dependencies: ["Translator"],
+            swiftSettings: strictSwiftSettings,
         ),
     ]
 )
+let strictSwiftSettings: [SwiftSetting] = [
+    .swiftLanguageMode(.v6),
+    .defaultIsolation(nil),
+    .strictMemorySafety(),
+]
